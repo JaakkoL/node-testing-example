c94fc7971cf1cc795fed41d4ca075010c5de0b33
JaakkoL <jaakko.o.laurila@gmail.com>
2019-11-25 09:58:06 +0200

    Handle StatusCodeErrors.

--- server/app/middleware/error-handler.js
+++ server/app/middleware/error-handler.js
@@ -1,5 +1,13 @@
+const { StatusCodeError } = require('request-promise/errors');
+
 module.exports = (err, req, res, next) => { // eslint-disable-line no-unused-vars
   const { id, ip, method, originalUrl } = req;
-  console.error(`${id} ${ip} ${method} ${originalUrl} - Unhandled error:`, err);
-  res.sendStatus(500);
+  if (err instanceof StatusCodeError) {
+    const { statusCode } = err;
+    console.error(`${id} ${ip} ${method} ${originalUrl} - StatusCodeError`, err.message);
+    res.sendStatus(statusCode);
+  } else {
+    console.error(`${id} ${ip} ${method} ${originalUrl} - Unhandled error:`, err.message);
+    res.sendStatus(500);
+  }
 };
